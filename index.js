bad457e84a61556477dff257db3792c4a5bb0ed8
Vachira J <cyberwizard.dev@gmail.com>
2020-11-29 01:39:46 +0700

    add new window

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {app, BrowserWindow, Menu} = electron;
 const isMac = process.platform === 'darwin';
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
@@ -12,6 +13,16 @@ app.on('ready', () => {
     Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo'
+    });
+
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+}
+
 const menuTemplate = [
     // {role: 'appMenu'}
     ...(isMac ? [{
@@ -20,7 +31,10 @@ const menuTemplate = [
     {
         label: 'File',
         submenu: [
-            { label: 'New Todo'},
+            { 
+                label: 'New Todo',
+                click() { createAddWindow(); }
+            },
             { 
                 label: 'Quit',
                 accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
